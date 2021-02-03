a6c37c930d65a3dcb6bbb97d18b4d7775b98d9b5
Soumik Nandi <soumik.ss.ml@gmail.com>
2021-02-04 03:10:04 +0530

    Added More Exanples

--- src/main/java/FactoryPattern/FactoryPatternDriver.java
+++ src/main/java/FactoryPattern/FactoryPatternDriver.java
@@ -6,9 +6,18 @@ import java.util.List;
 
 public class FactoryPatternDriver {
     public static void main (String args[]) throws Exception {
-        List lst = new ArrayList(Arrays.asList("ally", "Soumik"));
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(8, 6, 8, 1, 2));
+        List<String> list2 = new ArrayList<>(Arrays.asList("Ally", "Soumik", "Kumar"));
+        List<Double> list3 = new ArrayList<>(Arrays.asList(1.6,7.0,4.0,2.3));
+        List<Character> list4 = new ArrayList<>(Arrays.asList('a','y','d','a','s','h'));
+        //Producing Bubble Sort From Factory
         SortingAlgorithmFactory sortingAlgorithmFactory = new SortingAlgorithmFactory();
+
         //Sort Using Bubble Sort
-        System.out.println(sortingAlgorithmFactory.getSortingAlgorithm(SortingAlgorithmType.BUBBLE).sort(lst).toString());
+        SortingAlgorithm bubbleSort = sortingAlgorithmFactory.getSortingAlgorithm(SortingAlgorithmType.BUBBLE);
+        System.out.println(bubbleSort.sort(list1).toString());
+        System.out.println(bubbleSort.sort(list2).toString());
+        System.out.println(bubbleSort.sort(list3).toString());
+        System.out.println(bubbleSort.sort(list4).toString());
     }
 }
